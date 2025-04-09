fcd5839fd04c56c3b17df893c56f939c815d631f
Yido1007 <topcuyigithan@gmail.com>
2025-04-09 15:00:48 +0300

    API service was created with getx and functions were written for dio methods.

---
+++ lib/service/api.dart
@@ -0,0 +1,84 @@
+import 'package:dio/dio.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/instance_manager.dart';
+import 'package:trackwallet/service/storage.dart';
+
+// API için sabit değerlerin tanımlandığı soyut sınıf
+abstract class ApiConstant {
+  static const baseUrl = ""; // API'nin temel adresi burada tanımlanmalı
+}
+
+// GetX service class:
+class ApiService extends GetxService {
+  final Storage _storage = Get.find<Storage>();
+  late Dio _dio;
+
+  // Function that starts the service and makes Dio systems
+  Future<ApiService> init() async {
+    //Start Dio with basic settings
+    _dio = Dio(BaseOptions(
+      baseUrl: ApiConstant.baseUrl, // API adress
+      connectTimeout: Duration(seconds: 10), 
+      receiveTimeout: Duration(seconds: 10), 
+      contentType: "application/json" 
+    ));
+    // Interceptor that will run on every request
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        final token = _storage.getValue<String>(StorageKeys.userToken);
+        if (token != null) {
+          options.headers['Authorization'] = 'Bearer $token';
+        }
+        return handler.next(options);
+      },
+      onError: (error, handler) async {
+        if (error.response?.statusCode == 401) {
+          await _storage.remove(StorageKeys.userToken);
+        }
+        return handler.next(error);
+      },
+    ));
+
+    return this;
+  }
+
+  // Method to send GET request
+  Future<Response> get(String path, {Map<String, dynamic>? queryParameters, Options? options}) async {
+    try {
+      return await _dio.get(path, queryParameters: queryParameters, options: options);
+    } catch (e) {
+      print("Dio get error $e"); 
+      rethrow; 
+    }
+  }
+
+  // Method to send Post request
+  Future<Response> post(String path, {dynamic data, Map<String, dynamic>? queryParameters, Options? options}) async {
+    try {
+      return await _dio.post(path, data: data, queryParameters: queryParameters, options: options);
+    } catch (e) {
+      print("Dio post error $e");
+      rethrow;
+    }
+  }
+
+  // Method to send Put request
+  Future<Response> put(String path, {dynamic data, Map<String, dynamic>? queryParameters, Options? options}) async {
+    try {
+      return await _dio.put(path, data: data, queryParameters: queryParameters, options: options);
+    } catch (e) {
+      print("Dio put error $e");
+      rethrow;
+    }
+  }
+
+  // Method to send Delete request
+  Future<Response> delete(String path, {dynamic data, Map<String, dynamic>? queryParameters, Options? options}) async {
+    try {
+      return await _dio.delete(path, data: data, queryParameters: queryParameters, options: options);
+    } catch (e) {
+      print("Dio delete error $e");
+      rethrow;
+    }
+  }
+}
